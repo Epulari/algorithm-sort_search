06fa1ef2ef36406e036b53041343d55cf8bc3a6e
Epulari T 5818 <ruoxifei@gmail.com>
2018-08-02 16:59:25 +0800

    insertion_sort

---
+++ sort/sort_csharp/sort_csharp/InsertionSort.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics; //定义计时对象
+
+namespace sort_csharp
+{
+    /*
+     * 功能
+     * 插入排序
+     * Epulari T 
+     * 2018.8.2
+     */
+    public class InsertionSort
+    {
+        /// <summary>
+        /// 插入排序
+        /// </summary>
+        /// <param name="arr">arr</param>
+        public void MyInsertionSort(int[] arr)
+        {
+            Stopwatch oTime = new Stopwatch(); //定义一个计时对象
+            oTime.Start(); //开始计时 
+
+            //外层循环：一轮比较。每次已排序列多一个元素，i代表已排序列元素的最大索引，最开始只有一个元素arr[0]
+            for (int i = 0, len = arr.Length; i < len - 1; i++)
+            {
+                //内层循环：进行每轮的单个元素比较。待排元素为已排元素的下一个值arr[i + 1]，即arr[j]，将之与已排元素进行比较
+                for (int j = i + 1; j > 0; j--)
+                {
+                    if (arr[j] < arr[j - 1])
+                    {
+                        var temp = arr[j - 1];
+                        arr[j - 1] = arr[j];
+                        arr[j] = temp;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+
+                //输出本轮排序结果，字符以空格间隔
+                foreach (int k in arr)
+                {
+                    Console.Write(k + " ");
+                }
+                //换行，每轮为一行
+                Console.WriteLine();
+            }
+
+            oTime.Stop(); //结束计时
+            Console.WriteLine("程序的运行时间：{0} 秒", oTime.Elapsed.TotalSeconds);
+        }
+    }
+}
